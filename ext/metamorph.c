1727c638be76171682b21215d7f02b17ac765f08
Loren Segal <lsegal@soen.ca>
2010-03-25 20:42:03 -0400

    Fix whitespace

--- ext/metamorph.c
+++ ext/metamorph.c
@@ -7,8 +7,8 @@ static VALUE
 rb_mod_add_method(VALUE mod, VALUE name, VALUE body)
 {
 	void *node = rb_method_body(body);
-    if (node) rb_add_method(mod, rb_to_id(name), node, 0);
-    return body;
+	if (node) rb_add_method(mod, rb_to_id(name), node, 0);
+	return body;
 }
 
 void
